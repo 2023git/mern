fe1339e5ee68b53d1c572389afb0f16f062d4e1d
Lim Mikhail <129537885+2023git@users.noreply.github.com>
2023-05-20 04:37:02 +0600

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+//   baseURL: "http://localhost:5000/api",
+    baseURL: "http://5.181.109.74:5000/api",
 });
 
 instance.interceptors.request.use((config) => {
